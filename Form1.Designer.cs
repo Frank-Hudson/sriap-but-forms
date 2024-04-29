5ae5abd7412449aedbe6a9fcd05d0eddc69c6adf
Frank Hudson <349984@cymoedd.ac.uk>
2024-04-29 10:08:22 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace SriapButForms
+{
+	partial class formforGameplay
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// formforGameplay
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "formforGameplay";
+			this.Text = "Sriap";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
